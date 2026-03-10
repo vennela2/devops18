debddcb11adc87462fc8f8e11e1b447eb1c2b3c0
vennela2 <67159241+vennela2@users.noreply.github.com>
2026-03-10 16:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-02dfbd4ff395f2a1b"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "venny"
     tags = {
         Name = "DevOps"
     }
